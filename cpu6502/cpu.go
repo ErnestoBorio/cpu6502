f4bcb9fbdff9852438d5e71db49a55dcfd03142d
Ernesto Borio <ernestoborio@gmail.com>
2019-08-26 19:05:16 -0300

    Comments

--- cpu6502/cpu.go
+++ cpu6502/cpu.go
@@ -29,9 +29,10 @@ type MemoryReader = func(word) byte
 type MemoryWriter = func(word, byte)
 
 // Initialize the state of the cpu as stated in:
-// http://wiki.nesdev.com/w/index.php/CPU_power_up_state
+// http://wiki.nesdev.com/w/index.php/CPU_power_up_state 
+// https://www.c64-wiki.com/wiki/Reset_(Process)
 func (cpu *Cpu) Init() *Cpu {
-	cpu.Stack = 0xFD
+	cpu.Stack = 0xFD // because of a fake push of PC and flags
 	cpu.A = 0
 	cpu.X = 0
 	cpu.Y = 0
